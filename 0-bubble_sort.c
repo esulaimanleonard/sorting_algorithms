f1f575e5c31cd74f8838f92ad45dcd9a41e7ec0d
esulaimanleonard <111577374+esulaimanleonard@users.noreply.github.com>
2023-01-27 12:18:01 +0000

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,19 +7,19 @@
 void bubble_sort(int *array, size_t size)
 {
 
-	size_t i, index, tmp = 0;
+size_t i, index, tmp = 0;
 
-	if (size < 2)
-		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
-		{
-			if (array[index] > array[index + 1] && array[index + 1])
-			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
-			}
-		}
+if (size < 2)
+return;
+for (i = 0; i < size; i++)
+for (index = 0; index < size; index++)
+{
+if (array[index] > array[index + 1] && array[index + 1])
+{
+tmp = array[index];
+array[index] = array[index + 1];
+array[index + 1] = tmp;
+print_array(array, size);
+}
+}
 }
